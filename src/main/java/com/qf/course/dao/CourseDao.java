c9785e8ad04a9abcee45dd757c6a1c586e73b1d7
h929652784 <1062348176@qq.com>
2018-10-18 16:14:29 +0800

    选课提交

---
+++ src/main/java/com/qf/course/dao/CourseDao.java
@@ -0,0 +1,24 @@
+package com.qf.course.dao;
+
+import com.alibaba.fastjson.JSONObject;
+import com.qf.model.Course;
+import com.qf.model.Select_course;
+
+
+import java.util.List;
+
+public interface CourseDao {
+
+    int saveCourse(Course course);
+
+    int deleteCourse(Integer course_id);
+
+    int updateCourse(Course course);
+
+    Long selectCount(JSONObject jsonObject);
+
+    List<Course> selectByLimitAndSearch(JSONObject jsonObject);
+
+    Course selectByCourseId(Integer  course_id);
+
+}
